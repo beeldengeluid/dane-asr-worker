7862967d541030fc767b816d2eeb1f72f39e63ef
Jaap Blom <jblom@beeldengeluid.nl>
2020-12-02 15:39:48 +0100

    name param to ASR container

--- start-container.sh
+++ start-container.sh
@@ -13,4 +13,5 @@ docker run --rm -v $PWD/mount/input-files:/input-files \
 	-v $PWD/mount/output-files:/output-files \
 	-v $PWD/mount/asr-output:/asr-output \
 	-p $DANE_DOCKER_PORT:$DANE_DOCKER_PORT \
+	--name dane-asr \
 	$DANE_DOCKER_IMAGE
