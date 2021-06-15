1b2bc90518b8a6c8c325b3a8699d16eb0f39e10e
Jaap Blom <jblom@beeldengeluid.nl>
2021-06-15 12:05:58 +0200

    put all process ID stuff in work_processor; now also putting http response body in pid file for convenience and clarity

--- src/docker_src/apis/process/process_api.py
+++ src/docker_src/apis/process/process_api.py
@@ -1,12 +1,10 @@
-import requests
-import json
 import threading
 import os
-from flask import current_app, request, Response
-from flask_restx import Api, Namespace, Resource, fields
+from flask import request
+from flask_restx import Namespace, Resource, fields
 import logging
-from settings import LOG_NAME, PID_CACHE_DIR, MAIN_INPUT_DIR
-from work_processor import process_input_file, run_simulation
+from settings import LOG_NAME, MAIN_INPUT_DIR
+from work_processor import process_input_file, run_simulation, poll_pid_status
 
 api = Namespace('ASR Processing API', description='Process mp3 & wav into text')
 
@@ -48,39 +46,32 @@ status code natively...
 })
 class ProcessEndpoint(Resource):
 
-	def get_pid_file_name(self, pid):
-		return '{0}/{1}'.format(PID_CACHE_DIR, pid)
+	#@api.response(200, 'Success', processResponse)
+	def put(self, pid):
+		input_file = request.args.get('input_file', None)
+		wait = request.args.get('wait_for_completion', '1') == '1'
+		simulate = request.args.get('simulate', '1') == '1'
+		if input_file:
+			if wait:
+				resp = self._process(pid, input_file, simulate)
+			else:
+				resp = self._process_async(pid, input_file, simulate)
+			return resp, resp['state'], {}
+		else:
+			return {'state' : 400, 'message' : 'error: bad params'}, 400, {}
+		return {'state' : 500, 'message' : 'error: internal server error'}, 500, {}
 
-	def pid_file_exists(self, pid):
-		return os.path.exists(self.get_pid_file_name(pid))
+	#fetch the status of the pid
+	def get(self, pid):
+		return poll_pid_status(pid)
 
-	def create_pid_file(self, pid):
-		f  = open(self.get_pid_file_name(pid), 'w+')
-		f.close()
-
-	def write_pid_file(self, pid, txt):
-		f  = open(self.get_pid_file_name(pid), 'w')
-		f.write(txt)
-		f.close()
-
-	def read_pid_file(self, pid):
-		f  = open(self.get_pid_file_name(pid), 'r')
-		txt = ''
-		for l in f.readlines():
-			txt += l
-		f.close()
-		return txt
-
-	#process in a different thread, so the client immediately gets a response and can start polling progress via GET
-	def process_async(self, pid, input_file, simulate=True):
-		logger.debug('Processing mode = async; saving the pid to file')
-		self.create_pid_file(pid)
+		#process in a different thread, so the client immediately gets a response and can start polling progress via GET
+	def _process_async(self, pid, input_file, simulate=True):
 		logger.debug('starting ASR in different thread...')
-		t = threading.Thread(target=self.process, args=(
+		t = threading.Thread(target=self._process, args=(
 			pid,
 			input_file,
 			simulate,
-			self.get_pid_file_name(pid),
 			True,
 		))
 		t.daemon = True
@@ -89,53 +80,15 @@ class ProcessEndpoint(Resource):
 		#return this response, so the client knows it can start polling
 		return {
 			'state' : 200,
-			'message' : 'submitted the ASR work; status can be retrieved via PID={0}'.format(pid),
+			'message' : 'submitted the ASR work; status can be retrieved via PID={}'.format(pid),
 			'pid' : pid
-		}, 200, {}
+		}
 
-	def process(self, pid, input_file, simulate=True, pid_file=None, asynchronous=False):
+	def _process(self, pid, input_file, simulate=True, asynchronous=False):
 		logger.debug('Starting ASR in same thread...')
 		logger.debug('running asr for PID={}'.format(pid))
 		if simulate:
-			resp = run_simulation(os.path.join(MAIN_INPUT_DIR, input_file))
+			resp = run_simulation(pid, os.path.join(MAIN_INPUT_DIR, input_file), asynchronous)
 		else:
-			resp = process_input_file(os.path.join(MAIN_INPUT_DIR, input_file))
-
-		#if in async mode make sure to set the status file to "done"/"failed", so the client poller knows
-		if asynchronous:
-			success = 'state' in resp and resp['state'] == 200
-			logger.debug('updating pid file {0}'.format(pid))
-			f  = open(pid_file, 'w')
-			f.write('done' if success else 'failed')
-			f.close()
-
+			resp = process_input_file(pid, os.path.join(MAIN_INPUT_DIR, input_file), asynchronous)
 		return resp
-
-	#@api.response(200, 'Success', processResponse)
-	def put(self, pid):
-		input_file = request.args.get('input_file', None)
-		wait = request.args.get('wait_for_completion', '1') == '1'
-		simulate = request.args.get('simulate', '1') == '1'
-		if input_file:
-			if wait:
-				resp = self.process(pid, input_file, simulate)
-			else:
-				resp = self.process_async(pid, input_file, simulate)
-			return resp, resp['state'], {}
-		else:
-			return {'state' : 400, 'message' : 'error: bad params'}, 400, {}
-		return {'state' : 500, 'message' : 'error: internal server error'}, 500, {}
-
-	#fetch the status of the pid
-	def get(self, pid):
-		logger.debug('Getting status for pid {0}'.format(pid))
-		if not self.pid_file_exists(pid):
-			return {'state' : 404, 'message' : 'Error: PID does not exist (anymore)'}, 404, {}
-
-		status = self.read_pid_file(pid)
-		if status == 'done':
-			return {'state' : 200, 'message' : 'finished'}, 200, {}
-		elif status == 'failed':
-			return {'state' : 500, 'message' : 'failed'}, 200, {}
-		else:
-			return {'state' : 200, 'message' : 'in progress'}, 200, {}
--- src/docker_src/work_processor.py
+++ src/docker_src/work_processor.py
@@ -2,10 +2,11 @@ import os
 import ntpath
 import shutil
 import logging
+import json
 from time import sleep
 from asr import run_asr, process_asr_output, create_word_json
 from transcode import transcode_to_mp3
-from settings import LOG_NAME
+from settings import LOG_NAME, PID_CACHE_DIR
 
 """
 This module contains all the specific processing functions for the DANE-asr-worker. The reason
@@ -20,20 +21,27 @@ The processing consists of:
 
 logger = logging.getLogger(LOG_NAME)
 
-def run_simulation(input_file_path):
+#simulates running ASR and/or transcoding, so the API communication can be tested in isolation
+def run_simulation(pid, input_file_path, asynchronous=False):
 	logger.debug('simlating ASR, but verifying the validity of the request params')
 	logger.debug(input_file_path)
 	if not os.path.isfile(input_file_path):  # check if inputfile exists
-		return {'state': 404, 'message': 'No file found at file location {0}'.format(input_file_path)}
+		return _resp_to_pid_file(pid, asynchronous, {
+			'state': 404, 'message': 'No file found at file location {0}'.format(input_file_path)
+		})
 	else:
 		sleep(5)
-		return {'state' : 200, 'message' : 'Succesfully ran ASR on {0}'.format(input_file_path)} # assume the best
+		return _resp_to_pid_file(pid, asynchronous, {
+			'state' : 200, 'message' : 'Succesfully ran ASR on {0}'.format(input_file_path)
+		})
 
-def process_input_file(input_file_path):
-	print('analyzing the input file path')
-
+#processes the input and keeps a PID file with status information in asynchronous mode
+def process_input_file(pid, input_file_path, asynchronous=False):
+	logger.debug('processing {} for PID={}'.format(input_file_path, pid))
 	if not os.path.isfile(input_file_path):  # check if inputfile exists
-		return {'state': 404, 'message': 'No file found at file location {0}'.format(input_file_path)}
+		return _resp_to_pid_file(pid, asynchronous, {
+			'state': 404, 'message': 'No file found at file location {0}'.format(input_file_path)
+		})
 
 	#grab the file_name from the path
 	file_name = ntpath.basename(input_file_path)
@@ -45,8 +53,8 @@ def process_input_file(input_file_path):
 	asr_input_path = input_file_path
 
 	#check if the input file is a valid audio file, if not: transcode it first
-	if not is_audio_file(extension):
-		if is_transcodable(extension):
+	if not _is_audio_file(extension):
+		if _is_transcodable(extension):
 			asr_input_path = os.path.join(
 				os.sep,
 				os.path.dirname(input_file_path), #the dir the input file is in
@@ -57,31 +65,44 @@ def process_input_file(input_file_path):
 				asr_input_path #the transcode output is the input for the ASR
 			)
 		else:
-			return {
+			return _resp_to_pid_file(pid, asynchronous, {
 				'state': 406,
 				'message': 'Not acceptable: accepted file formats are; mov,mp4,m4a,3gp,3g2,mj2'
-			}
+			})
 
 	#run the ASR
 	try:
 		run_asr(asr_input_path, asset_id)
 	except Exception as e:
-		return {
+		return _resp_to_pid_file(pid, asynchronous, {
 			'state': 500,
 			'message': 'Something went wrong when encoding the file: {0}'.format(e)
-		}
+		})
 
 	#finally process the ASR results and return the status message
-	return process_asr_output(asset_id)
+	return _resp_to_pid_file(pid, asynchronous, process_asr_output(asset_id))
 
-def is_audio_file(extension):
+def poll_pid_status(pid):
+	logger.debug('Getting status for pid {}'.format(pid))
+	if not _pid_file_exists(pid):
+		return {'state' : 404, 'message' : 'Error: PID does not exist (anymore)'}, 404, {}
+
+	status = _read_pid_file(pid)
+	if status == 'done':
+		return {'state' : 200, 'message' : 'finished'}, 200, {}
+	elif status == 'failed':
+		return {'state' : 500, 'message' : 'failed'}, 200, {}
+	else:
+		return {'state' : 200, 'message' : 'in progress'}, 200, {}
+
+def _is_audio_file(extension):
 	return extension in ['.mp3', '.wav']
 
-def is_transcodable(extension):
+def _is_transcodable(extension):
 	return extension in [".mov", ".mp4", ".m4a", ".3gp", ".3g2", ".mj2"]
 
 #clean up input files
-def remove_files(path):
+def _remove_files(path):
 	for file in os.listdir(path):
 		file_path = os.path.join(path, file)
 		try:
@@ -91,3 +112,34 @@ def remove_files(path):
 				shutil.rmtree(file_path)
 		except Exception as e:
 			print('Failed to delete {0}. Reason: {1}'.format(file_path, e))
+
+"""-------------------------------------- PID FILE FUNCTIONS -----------------------------------------------"""
+
+#writes the API response to the PID file, if the "ASR job" is running asynchronously
+def _resp_to_pid_file(pid, asynchronous, resp):
+	if asynchronous:
+		_write_pid_file(pid, json.dumps(resp))
+	return resp
+
+def _get_pid_file_name(pid):
+	return '{}/{}'.format(PID_CACHE_DIR, pid)
+
+def _pid_file_exists(pid):
+	return os.path.exists(_get_pid_file_name(pid))
+
+def _create_pid_file(pid):
+	f  = open(_get_pid_file_name(pid), 'w+')
+	f.close()
+
+def _write_pid_file(pid, txt):
+	f  = open(_get_pid_file_name(pid), 'w')
+	f.write(txt)
+	f.close()
+
+def _read_pid_file(pid):
+	f  = open(_get_pid_file_name(pid), 'r')
+	txt = ''
+	for l in f.readlines():
+		txt += l
+	f.close()
+	return txt
