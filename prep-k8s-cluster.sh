60fd59118726a873b1beb4422c4f721a3c5fe515
Jaap Blom <jblom@beeldengeluid.nl>
2021-07-07 10:01:28 +0200

    add configmap mount for kaldi api in k8s yaml

--- prep-k8s-cluster.sh
+++ prep-k8s-cluster.sh
@@ -13,3 +13,9 @@ docker build -t dane-server-api -f Dockerfile.api .
 # prepare the cluster by creating a volume the ES endpoint (current DANE cluster)
 
 kubectl apply -f k8s-cluster-requirements.yaml
+
+
+# create the configmaps (first make sure you have these settings!)
+
+kubectl create configmap dane-asr-worker-cfg --from-file config.yml
+kubectl create configmap dane-kaldi-api-cfg --from-file asr_api/config/settings.py
