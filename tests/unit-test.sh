b6b529b8c7915a6c57df088bae68afd836d81741
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2022-03-23 11:16:36 +0100

    Use pyproject.toml to configure black

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -19,5 +19,5 @@ pipenv run flake8 . --count --select=E9,F63,F7,F82,W191 --show-source --statisti
 # exit-zero treats all errors as warnings
 pipenv run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics --extend-ignore=E203,E501 --select=C,E,F,W,B,B950
 
-# check formatting
+# check formatting (configured in pyproject.toml)
 black --check .
