ef78bf9feec1104845b376358f76736092a0cf2d
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2022-03-23 12:02:36 +0100

    Remove unnecesary unstub

--- tests/unit/base_util_test.py
+++ tests/unit/base_util_test.py
@@ -1,9 +1,5 @@
-from mockito import unstub
 from base_util import validate_config
 
 
 def test_validate_config(config, environment_variables):
-    try:
-        assert validate_config(config, False)
-    finally:
-        unstub()
+    assert validate_config(config, False)
