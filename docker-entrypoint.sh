cdc69c511f3b6439eb71f603a1ab8c93e1a99086
Jaap Blom <jblom@beeldengeluid.nl>
2023-10-19 11:34:09 +0200

    tail /dev/null to enable debug after crash and invisible self heals

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ echo python3 is located here: `which python3`
 echo poetry is located here: `which poetry`
 echo virtualenv is in: `poetry env info --path`
 
-# done now tailing to debug
-# tail -f /dev/null
-
 echo "Now starting the worker"
 poetry run python3 worker.py
+
+echo worker crashed, tailing /dev/null for debugging
+tail -f /dev/null
